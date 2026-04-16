cf6af0e44de6b2c142b07f70b55ab6f357a19baf
PedroSales1505 <pedrosales1505@gmail.com>
2026-04-16 17:20:23 -0300

    ATIVIDADE

--- JavaScript/ATIVIDADE AULA 1/scripts/Sucessor.js
+++ JavaScript/ATIVIDADE AULA 1/scripts/Sucessor.js
@@ -1,6 +1,6 @@
-var NumeroDigitado= parseInt(prompt ("Digite um número"))
-var NumeroAntecessor = NumeroDigitado -1
-var NumeroSucessor = NumeroDigitado +1
+var NumeroDigitado = parseInt(prompt("Digite um número"))
+var NumeroAntecessor = NumeroDigitado - 1
+var NumeroSucessor = NumeroDigitado + 1
 document.getElementById.innerHTML = `<h1> Certo, esses são os valores em ordem: </h1>`;
 document.getElementById("NumeroAntecessor").innerHTML = `<h1> ${NumeroAntecessor}</h1>`;
 document.getElementById("NumeroDigitado").innerHTML = `<h1> ${NumeroDigitado}</h1>`;
