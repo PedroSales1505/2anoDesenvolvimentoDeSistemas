d8069c6feb30bf2c24c010d134b97a3c1e9de842
PedroSales1505 <pedrosales1505@gmail.com>
2026-06-09 18:14:10 -0300

    ATIVIDADE

---
+++ JavaScript/ATIVIDADE AULA 7/scripts/script.js
@@ -0,0 +1,15 @@
+const nome = "Pedro"
+const idade = 17
+const online = true
+const partidas = [2, 3, 4]
+
+let jogoFavorito = {
+    nome:"Resident evil 4 Remake", 
+    anoLancamento: 2023
+}
+
+
+console.log(online, typeof online)
+console.log(idade,typeof idade)
+console.log(nome,typeof nome)
+console.log(partidas,typeof partidas)
