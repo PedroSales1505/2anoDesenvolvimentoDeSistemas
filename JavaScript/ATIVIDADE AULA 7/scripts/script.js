91a9d195f90b9d03c0b5dbdf6120c1a1e9b604a5
PedroSales1505 <pedrosales1505@gmail.com>
2026-06-16 16:46:38 -0300

    ...

--- JavaScript/ATIVIDADE AULA 7/scripts/script.js
+++ JavaScript/ATIVIDADE AULA 7/scripts/script.js
@@ -1,15 +1,25 @@
-const nome = "Pedro"
-const idade = 17
-const online = true
-const partidas = [2, 3, 4]
+const nomeJogador = "João Gamer"; // constante (não pode alterar)
+let idade = 16;    //number
+let online = true; //boolean
 
+// Objeto com o jogo favorito
 let jogoFavorito = {
-    nome:"Resident evil 4 Remake", 
-    anoLancamento: 2023
-}
+    nome: "GTA V",
+    anoLancamento: 2013
+};
 
+// Array com últimas 3 potuações
+let pontuacoes = [1500, 2000, 1800];
 
-console.log(online, typeof online)
-console.log(idade,typeof idade)
-console.log(nome,typeof nome)
-console.log(partidas,typeof partidas)
+// Mostrando os valores e seus tipos no console
+console.log("Nome:", nomeJogador, "Tipo:", typeof nomeJogador);
+console.log("Idade:", idade, "Tipo:", typeof idade);
+console.log("Online:", online, "Tipo:", typeof online);
+console.log("Jogo Favorito:", jogoFavorito, "Tipo:", typeof jogoFavorito);
+console.log("Pontuações:", pontuacoes, "Tipo:", typeof pontuacoes);
+
+// Mudando valores da idade e do status online
+idade = 17;
+online = false;
+console.log("Nova idade:", idade);
+console.log("Novo status online:", online);
